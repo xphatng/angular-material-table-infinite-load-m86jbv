8c621c54489f84841abb65e70be9f7fac29cf200
xphatng <tanphatcta@gmail.com>
2022-08-24 16:34:25 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-table-infinite-load-m86jbv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-table-infinite-load-m86jbv)
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatTableModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
